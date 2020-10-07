698d07106bbd8986ad9cafc03c8d92aab351f4a8
Kendy JEAN-MARIE <kendyjm@gmail.com>
2020-10-07 08:28:17 +0200

    feat: add lambda function GetListingsUser

--- backend/src/lambda/http/getListingsUser.ts
+++ backend/src/lambda/http/getListingsUser.ts
@@ -3,7 +3,8 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 import { createLogger } from '../../utils/logger'
 import { getUser } from '../utils'
-import { getListings } from '../../business/ListingsBusiness'
+import { getListingsUser } from '../../business/ListingsBusiness'
+import { User } from '../../auth/user'
 
 const logger = createLogger('getListings-function')
 
@@ -11,10 +12,10 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
   logger.debug("Processing event", event)
 
   // check authorization
-  const user = getUser(event)
-  logger.info(`Get all the listings`)
+  const user: User = getUser(event)
+  logger.info("Get listings of user", user)
 
-  const items = await getListings();
+  const items = await getListingsUser(user);
 
   return {
     statusCode: 200,
